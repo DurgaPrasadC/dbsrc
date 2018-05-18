51ad3b86ff0470d463c1f2d946335bb8752364f4
durgachamz <durgach@8c85902e2da4.ant.amazon.com>
2018-05-18 14:42:27 +0530

    updated sleep

--- dbcicd.sh
+++ dbcicd.sh
@@ -5,12 +5,12 @@ cd deployment-script
 
 echo "Invoking db creation script"
 python database_creation.py
-sleep 5s
+sleep 5
 
 echo "Invoking table creation script"
 python table_creation.py
-sleep 5s
+sleep 5
 
 echo "Invoking object creation script"
 python object_creation.py
-sleep 5s
+sleep 5
