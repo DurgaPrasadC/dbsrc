9dbbfcbf44c48c76e7d5fe3fba79ea5c97c679e3
durgachamz <durgach@8c85902e2da4.ant.amazon.com>
2018-05-23 17:02:18 +0530

    updated with changes only deployment logic

---
+++ changescicd.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+export /usr/bin/psql
+cd /home/ec2-user/gitsrc/dbsrc
+cat /dev/null > changescicd_error_log.log
+git pull
+files=`git diff --name-only HEAD@{0} HEAD@{1} | grep .sql`
+for file in $files; do
+  psql -d testdb -U testdb -h pgdbinstance.c3t0g5fpfejj.us-east-1.rds.amazonaws.com -p 5432 -f $file >> changescicd_error_log.log
+done
+
+errCount=$(tr -s ' ' '\n' < changescicd_error_log.log | grep ERROR | wc -l)
+if [ $errCount -gt 0 ]
+then
+   echo "Error occurred while applying changes to RDS since last git push."
+   cat changescicd_error_log.log
+   exit -1
+fi
+
