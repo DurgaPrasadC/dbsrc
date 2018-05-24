83e17d1019a02425a066baf7cddb6f7c8468c613
durgachamz <durgach@8c85902e2da4.ant.amazon.com>
2018-05-24 12:38:17 +0530

    updated shell with logs

--- changescicd.sh
+++ changescicd.sh
@@ -12,6 +12,7 @@ files=`git diff --name-only HEAD@{0} HEAD@{1} | grep .sql`
 echo "List of files in diff : $files" >> changescicd_error_log.log
 for file in $files; do
   echo "Current file being run : $file" >> changescicd_error_log.log
+  echo "pg pwd : $PGPASSWORD" >> changescicd_error_log.log
   psql -d testdb -h pgdbinstance.c3t0g5fpfejj.us-east-1.rds.amazonaws.com -p 5432 -U testdb -f $file >> changescicd_error_log.log
 done
 
