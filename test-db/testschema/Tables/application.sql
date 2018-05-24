61ab89d05d3a122a1aba8bdddb9955db41fedb4e
durgachamz <durgach@8c85902e2da4.ant.amazon.com>
2018-05-24 12:41:38 +0530

    updated shell

--- test-db/testschema/Tables/application.sql
+++ test-db/testschema/Tables/application.sql
@@ -2,7 +2,7 @@ DROP TABLE testschema.application;
 
 CREATE TABLE IF NOT EXISTS testschema.application(
     application_id                  INTEGER NOT NULL,
-    application_cd                  CHARACTER VARYING(201) NOT NULL
+    application_cd                  CHARACTER VARYING(204) NOT NULL
 )
 WITH (
 OIDS=FALSE
