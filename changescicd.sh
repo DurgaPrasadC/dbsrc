8a4ef8efd38a79fbbcddd3aab76fdfe4bae6091c
durgachamz <durgach@8c85902e2da4.ant.amazon.com>
2018-05-24 12:00:33 +0530

    updated shell

--- changescicd.sh
+++ changescicd.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 cd /home/ec2-user/gitsrc/dbsrc
+
 cat /dev/null > changescicd_error_log.log
 date >> changescicd_error_log.log
-git pull
+
+git pull origin
+sleep 5s
+
 files=`git diff --name-only HEAD@{0} HEAD@{1} | grep .sql`
 for file in $files; do
   psql -d testdb -h pgdbinstance.c3t0g5fpfejj.us-east-1.rds.amazonaws.com -p 5432 -U testdb -f $file >> changescicd_error_log.log
