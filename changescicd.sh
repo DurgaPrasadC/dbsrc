896f54b16cc9c05e0383ae769c082b49e91ae9c1
durgachamz <durgach@8c85902e2da4.ant.amazon.com>
2018-05-24 12:31:28 +0530

    udpated shell

--- changescicd.sh
+++ changescicd.sh
@@ -9,7 +9,9 @@ git pull origin
 sleep 5s
 
 files=`git diff --name-only HEAD@{0} HEAD@{1} | grep .sql`
+echo "List of files in diff : $files" >> changescicd_error_log.log
 for file in $files; do
+  echo "Current file being run : $file" >> changescicd_error_log.log
   psql -d testdb -h pgdbinstance.c3t0g5fpfejj.us-east-1.rds.amazonaws.com -p 5432 -U testdb -f $file >> changescicd_error_log.log
 done
 
